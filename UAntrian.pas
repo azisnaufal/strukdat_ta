2b2b82bec886b343bfb2d87bd8fea36bbf8f52a8
Azis Naufal <azis.naufal@gmail.com>
2019-08-01 04:12:56 +0700

    init

---
+++ UAntrian.pas
@@ -0,0 +1,28 @@
+program UAntrian;
+
+type
+  TInfo = record
+    antrian : String[8];
+  end;
+  PData = ^TData;
+
+  TData = record
+    info: TInfo;
+    Next: PData;
+  end;
+
+var
+  awal, akhir: PData;
+  pilihan: integer;
+
+//RUANG KERJA ALIF ↓↓
+
+
+//RUANG KERJA YUSUP ↓↓
+
+
+//RUANG KERJA AZIS ↓↓
+
+begin
+  
+end.
