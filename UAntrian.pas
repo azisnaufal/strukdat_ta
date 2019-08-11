3ac02c6d1a036b29466322860bf0ec2a20534e5c
Azis Naufal <azis.naufal@gmail.com>
2019-08-11 08:44:03 +0700

    ajis beres meurn

--- UAntrian.pas
+++ UAntrian.pas
@@ -14,6 +14,8 @@ type
 var
   awal, akhir: PData;
   pilihan: integer;
+  nomor_antrian_meja_1 : TInfo;
+  nomor_antrian_meja_2 : TInfo;
   banyak_data : integer;
 
 //RUANG KERJA ALIF ↓↓
@@ -36,7 +38,7 @@ end;
 
 //RUANG KERJA AZIS ↓↓
 
-//fungsi ini dipanggil untuk mengecek apakah nomor simpul merupakan daun (leaf) atau bukan
+//fungsi ini dipanggil untuk mengecek apakah nomor simpul merupakan daun (leaf) atau bukan (dibuat oleh azis)
 function apakahDaun(nomor_simpul : integer) : boolean;
 begin
   if (nomor_simpul >= (banyak_data div 2)) and (nomor_simpul <= banyak_data) then
@@ -45,19 +47,19 @@ begin
     apakahDaun := false;
 end;
 
-//fungsi ini dipanggil untuk mendapatkan posisi anak kiri dari nomor_simpul
+//fungsi ini dipanggil untuk mendapatkan posisi anak kiri dari nomor_simpul (dibuat oleh azis)
 function anakKiri(nomor_simpul : integer): integer;
 begin
   anakKiri := nomor_simpul * 2;
 end;
 
-//fungsi ini dipanggil untuk mendapatkan posisi anak kanan dari nomor_simpul
+//fungsi ini dipanggil untuk mendapatkan posisi anak kanan dari nomor_simpul (dibuat oleh azis)
 function anakKanan(nomor_simpul : integer): integer;
 begin
   anakKanan := nomor_simpul * 2 + 1;
 end;
 
-//fungsi ini dipanggil untuk mendapatkan posisi orangtua (parent) dari nomor_simpul
+//fungsi ini dipanggil untuk mendapatkan posisi orangtua (parent) dari nomor_simpul (dibuat oleh azis)
 function orangTua(nomor_simpul : integer): integer;
 begin
   orangTua := nomor_simpul div 2;
@@ -79,7 +81,7 @@ begin
   ambilNilaiDariHeap := bantu;
 end;
 
-//prosedur ini dipanggil untuk menukar data dalam heap
+//prosedur ini dipanggil untuk menukar data dalam heap (dibuat oleh azis)
 procedure tukar(nomor_simpul1:integer;nomor_simpul2:integer);
 var
   temp:TInfo;
@@ -89,7 +91,7 @@ begin
    ambilNilaiDariHeap(nomor_simpul2)^.info := temp;
 end;
 
-//prosedur ini dipanggil ketika ada penambahan data (dibuat oleh azis)
+//prosedur ini dipanggil ketika ada penambahan data geser_ke_atas = shift_up (dibuat oleh azis)
 procedure geser_ke_atas() ;
 var
   nomor_simpul : integer;
@@ -136,10 +138,56 @@ begin
   end;
 end;
 
+//hapus antrian
+function hapus_antrian() : TInfo;
+var
+  pHapus : PData;
+  info : TInfo;
+begin
+  info : awal^.info;
+  if (awal = akhir) then
+  begin
+    Dispose(awal);
+    awal := nil;
+    akhir := nil;
+  end
+  else
+  begin
+    pHapus := awal;
+    awal := awal^.Next;
+    Dispose(pHapus);
+  end;
+  hapus_antrian := info;
+end;
+
+function tampilMenu() : integer;
+var
+  pilihan : integer;
+begin
+    ClrScr;
+    WriteLn('======= Aplikasi Antrian Bank Masyarakat UNIKOM =======');
+    WriteLn('Meja 1 sedang melayani nomor antrian : ', nomor_antrian_meja_1.antrian);
+    WriteLn('Meja 2 sedang melayani nomor antrian : ', nomor_antrian_meja_2.antrian);
+    WriteLn('Nomor antrian selanjutnya : ', awal^.info.antrian);
+    WriteLn('1. Ambil nomor antrian bisnis');
+    WriteLn('2. Ambil nomor antrian personal');
+    WriteLn('3. Panggil nomor antrian ke meja 1');
+    WriteLn('4. Panggil nomor antrian ke meja 2');
+    WriteLn('5. Keluar aplikasi');
+    ReadLn(pilihan);
+    tampilMenu := pilihan;
+end;
+
 //MAIN FUNCTION ↓↓
 begin
-  sisip_belakang('coba',awal,akhir);
-  writeln(awal^.info.antrian);
   banyak_data := 0;
-  
+  repeat
+    pilihan := tampilMenu();
+    case(pilihan) of 
+      1 : ambilAntrianBisnis;
+      2 : ambilAntrianPersonal;
+      3 : panggilKeMeja1;
+      4 : panggilKeMeja2;
+    end;
+  until pilihan = 5
 end.
